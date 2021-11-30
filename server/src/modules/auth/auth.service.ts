0f33c0ad270b66a77ca837e965e69f497e0c031e
rec.fffixing <imdex1009@gmail.com>
2021-11-30 21:24:28 +0900

    Feat: Add Login Function
    ! 로그인 함수 구현 완료

--- server/src/modules/auth/auth.service.ts
+++ server/src/modules/auth/auth.service.ts
@@ -1,14 +1,47 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { AuthRepository } from "../../repositories/auth.repository";
+import { JwtService } from "@nestjs/jwt";
 import { AuthLoginDto } from "./dto/auth-login.dto";
+import * as bcrypt from "bcrypt";
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly authRepository: AuthRepository) {}
-  login(authLoginDto: AuthLoginDto) {
-    console.log(authLoginDto);
-    console.log(authLoginDto.userId);
-    return this.authRepository.login(authLoginDto);
+  constructor(
+    private readonly authRepository: AuthRepository,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async login(authLoginDto: AuthLoginDto) {
+    const userInfo = await this.authRepository.getUserByUserId(
+      authLoginDto.userId,
+    );
+    // 사용자 정보 확인하여 전달된 userId 검증
+    if (!userInfo) {
+      throw new UnauthorizedException("존재하지 않는 userId 입니다.");
+    }
+    // 패스워드 일치여부 확인
+    const passwordCheck = await bcrypt.compare(
+      authLoginDto.password,
+      userInfo.password,
+    );
+    // 패스워드 불일치 경우
+    if (!passwordCheck) {
+      throw new UnauthorizedException("password가 일치하지 않습니다");
+    }
+
+    // 토큰 발급을 위한 정보 구성
+    const payload: { [_id: string]: string } = { _id: userInfo._id.toString() };
+    // 토큰 생성
+    const accessToken: string = this.jwtService.sign(payload, {
+      expiresIn: "1d",
+    });
+
+    return { accessToken };
+  }
+
+  checkToken(accessToken: string) {
+    const result = this.jwtService.decode(accessToken);
+    return result;
   }
   // create(createAuthDto: CreateAuthDto) {
   //   return 'This action adds a new auth';
--- server/src/repositories/auth.repository.ts
+++ server/src/repositories/auth.repository.ts
@@ -4,7 +4,7 @@ import {
   NotAcceptableException,
   UnauthorizedException,
 } from "@nestjs/common";
-import { JwtService } from "@nestjs/jwt";
+
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { User } from "src/entities/user.entity";
@@ -13,30 +13,21 @@ import { AuthLoginDto } from "src/modules/auth/dto/auth-login.dto";
 export class AuthRepository {
   constructor(
     @InjectModel(User.name) private readonly userModel: Model<User>,
-    private readonly jwtService: JwtService,
   ) {}
-
-  async login(authLoginDto: AuthLoginDto) {
-    const userInfo = await this.getUserByUserId(authLoginDto.userId);
-    console.log(userInfo);
-
-    if (!userInfo) {
-      return new UnauthorizedException(
-        "Id에 해당되는 유저가 존재하지 않습니다",
-      );
-    }
-
-    if (authLoginDto.password !== userInfo.password) {
-      return new UnauthorizedException("비밀번호가 일치하지 않습니다.");
-    }
-
-    return "Success";
-  }
 
   // 사용자 ID 기준으로 유저 정보 조회
   async getUserByUserId(userId: string) {
     try {
       const user = await this.userModel.findOne({ userId });
+      return user;
+    } catch (err) {
+      throw new InternalServerErrorException(err);
+    }
+  }
+
+  async getUserByObjectId(_id: string) {
+    try {
+      const user = await this.userModel.findById(_id);
       return user;
     } catch (err) {
       throw new InternalServerErrorException(err);
