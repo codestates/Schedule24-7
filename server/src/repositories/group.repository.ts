32eef79ed6c11a0b919793e065f301405dc53ba1
SiwooPak <bogus1620@naver.com>
2021-12-06 13:15:52 +0900

    Feat: Add schedule_algo - repo, service edit

--- server/src/repositories/group.repository.ts
+++ server/src/repositories/group.repository.ts
@@ -1,7 +1,10 @@
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 
-import { InternalServerErrorException } from "@nestjs/common";
+import {
+  InternalServerErrorException,
+  NotFoundException,
+} from "@nestjs/common";
 
 import { Group } from "../entities/group.entity";
 import { User } from "src/entities/user.entity";
