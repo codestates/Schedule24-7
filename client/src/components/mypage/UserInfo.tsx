32cd223d9a1a31a5cde186783473a0995d166fb3
outclassstudio <mlimshs@gmail.com>
2021-12-07 11:47:48 +0900

    Feat: Add editpassword, signout feature

--- client/src/components/mypage/UserInfo.tsx
+++ client/src/components/mypage/UserInfo.tsx
@@ -1,14 +1,12 @@
 import { useState } from "react";
 import axios from "axios";
 import EditUser from "./EditUser";
-import { NormalBox, NormalBtn, MainLogo } from "../../style/theme";
+import { NormalBox, NormalBtn, MainLogo, ErrMsg } from "../../style/theme";
 import { useNavigate } from "react-router";
 import styled from "styled-components";
 import { useEffect } from "react";
 
 axios.defaults.withCredentials = true;
-
-// { handleOnMyPage, userId }
 
 export const UserInfoSection = styled.section`
   height: 100%;
@@ -37,7 +35,6 @@ export const UserInfoWrapper = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  /* background-color: #f9f9f9; */
 `;
 
 export const UserInfoItems = styled.div`
@@ -51,6 +48,10 @@ export const InfoHeader = styled.div`
   &.sub {
     font-size: 12px;
     font-weight: 340;
+    padding-left: 4px;
+  }
+  &.pwd {
+    padding-left: 4px;
   }
 `;
 
@@ -59,7 +60,6 @@ export const InfoBox = styled.div`
   height: 30px;
   padding-top: 15px;
   padding-left: 10px;
-  /* border-radius: 0.3rem; */
   border: 1px solid #a5a5a5;
   box-shadow: 0.05rem 0.05rem 0.05rem #6969692d;
   margin-bottom: 0.4rem;
@@ -75,16 +75,20 @@ function UserInfo() {
     navigate("/main");
   }
 
+  //패스워드, 변경할 패스워드, 변경할패스워드 확인
   const [newPassword, setPassword] = useState({
     password: "",
     newPassword: "",
     newPasswordCheck: "",
   });
+
+  //에러체크 및 에러메시지 관련 상태
   const [errorCheck, setError] = useState<boolean>(false);
   const [errorMessage, setErrorMessage] = useState<string>("");
   const [errorMessageCheck, setErrorMessageCheck] = useState<boolean>(false);
-  const [userInfo, setUserInfo] = useState<string>("");
+  const [userInfo, setUserInfo] = useState({ userName: "", email: "" });
 
+  //화면최초렌더링시 유저정보 불러옴
   useEffect(() => {
     axios
       .get("https://server.schedule24-7.link/users", {
@@ -93,8 +97,11 @@ function UserInfo() {
         },
       })
       .then((res) => {
-        // console.log(res);
-        setUserInfo(res.data.userName);
+        setUserInfo({
+          ...userInfo,
+          userName: res.data.userName,
+          email: res.data.email,
+        });
       });
   }, []);
 
@@ -105,53 +112,51 @@ function UserInfo() {
       console.log(newPassword.newPassword);
     };
 
+  //패스워드 일치여부 확인 함수
   const checkPassword = () => {
-    // axios
-    //   .get(
-    //     `https://server.webmarker.link/users/password/${newPassword.password}`,
-    //     {
-    //       headers: {
-    //         authorization: `Bearer ${window.localStorage.getItem("token")}`,
-    //       },
-    //     }
-    //   )
-    //   .then((res) => {
-    //     if (res.data.data.result) {
-    //       setErrorMessageCheck(false);
-    //       setError(res.data.data.result);
-    //     } else {
-    //       setErrorMessageCheck(true);
-    //       setErrorMessage("비밀번호가 일치하지 않습니다");
-    //     }
-    //   });
-    setError(true);
+    axios
+      .get(
+        `https://server.schedule24-7.link/auth/password/${newPassword.password}`,
+        {
+          headers: {
+            authorization: `Bearer ${window.localStorage.getItem("token")}`,
+          },
+        }
+      )
+      .then((res) => {
+        if (res.data.result) {
+          setErrorMessageCheck(false);
+          setError(res.data.result);
+        } else {
+          setErrorMessageCheck(true);
+          setErrorMessage("비밀번호가 일치하지 않습니다");
+        }
+      });
   };
-  //패스워드 체크하는 함수
 
-  const handleChangePassowrd = () => {
-    // if (newPassword.newPassword === newPassword.newPasswordCheck) {
-    //   axios
-    //     .patch(
-    //       "http://ec2-54-180-96-63.ap-northeast-2.compute.amazonaws.com/users/password",
-    //       { password: newPassword.newPassword },
-    //       {
-    //         headers: {
-    //           Authorization: `Bearer ${window.localStorage.getItem("token")}`,
-    //         },
-    //       }
-    //     )
-    //     .then(() => {
-    //       alert("비밀번호가 성공적으로 변경되었습니다");
-    //       setErrorMessageCheck(false);
-    //       setError(false);
-    //     });
-    // } else {
-    //   setErrorMessageCheck(true);
-    //   setErrorMessage("비밀번호가 일치하지 않습니다");
-    // }
-    return;
-  };
   //비밀번호 변경 요청하는 함수
+  const handleChangePassowrd = () => {
+    if (newPassword.newPassword === newPassword.newPasswordCheck) {
+      axios
+        .patch(
+          "https://server.schedule24-7.link/users",
+          { new_password: newPassword.newPassword },
+          {
+            headers: {
+              Authorization: `Bearer ${window.localStorage.getItem("token")}`,
+            },
+          }
+        )
+        .then(() => {
+          alert("비밀번호가 성공적으로 변경되었습니다");
+          setErrorMessageCheck(false);
+          setError(false);
+        });
+    } else {
+      setErrorMessageCheck(true);
+      setErrorMessage("비밀번호가 일치하지 않습니다");
+    }
+  };
 
   return (
     <UserInfoSection>
@@ -163,24 +168,22 @@ function UserInfo() {
         <form onSubmit={(e) => e.preventDefault()}>
           <UserInfoWrapper>
             <UserInfoItems>
-              {/* <h2>회원정보</h2> */}
               <InfoHeader>아이디</InfoHeader>
               <InfoBox>{window.localStorage.getItem("userId")}</InfoBox>
             </UserInfoItems>
             <UserInfoItems>
               <InfoHeader>사용자이름</InfoHeader>
-              <InfoBox>{userInfo}</InfoBox>
+              <InfoBox>{userInfo.userName}</InfoBox>
             </UserInfoItems>
             <UserInfoItems>
-              <InfoHeader>비밀번호변경 및 탈퇴</InfoHeader>
+              <InfoHeader>이메일</InfoHeader>
+              <InfoBox>{userInfo.email}</InfoBox>
+            </UserInfoItems>
+            <UserInfoItems>
+              <InfoHeader className="pwd">비밀번호변경 및 탈퇴</InfoHeader>
               <InfoHeader className="sub">
                 * 계정정보확인 후 가능합니다
               </InfoHeader>
-              <NormalBox
-                type="email"
-                placeholder="email"
-                onChange={handlePassword("password")}
-              />
               <NormalBox
                 type="password"
                 placeholder="비밀번호"
@@ -192,14 +195,14 @@ function UserInfo() {
         </form>
         {errorCheck ? (
           <EditUser
-          //   errorMessageCheck={errorMessageCheck}
-          //   handlePassword={handlePassword}
-          //   handleChangePassowrd={handleChangePassowrd}
+            newPassword={newPassword}
+            handlePassword={handlePassword}
+            handleChangePassowrd={handleChangePassowrd}
           />
         ) : (
           ""
         )}
-        {errorMessageCheck ? <div>{errorMessage}</div> : ""}
+        {errorMessageCheck ? <ErrMsg>{errorMessage}</ErrMsg> : ""}
       </UserInfoDiv>
     </UserInfoSection>
   );
