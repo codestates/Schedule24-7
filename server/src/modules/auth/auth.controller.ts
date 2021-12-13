d088f26f75c25147a415faf4ebaa5ac3f23205f7
rec.fffixing <imdex1009@gmail.com>
2021-12-13 15:35:43 +0900

    Feat: Edit redirect url

--- server/src/modules/auth/auth.controller.ts
+++ server/src/modules/auth/auth.controller.ts
@@ -329,7 +329,7 @@ export class AuthController {
   async googleAuth(@Res() res: Response) {
     const GOOGLE_AUTH_URL = "https://accounts.google.com/o/oauth2/v2/auth";
     const GOOGLE_AUTH_REDIRECT_URL =
-      "http://server.schedule24-7.link/auth/google/callback";
+      "https://server.schedule24-7.link/auth/google/callback";
     try {
       return res.redirect(
         `${GOOGLE_AUTH_URL}?client_id=${process.env.GOOGLE_AUTH_CLIENT_ID}&redirect_uri=${GOOGLE_AUTH_REDIRECT_URL}&response_type=code&include_granted_scopes=true&scope=https://www.googleapis.com/auth/userinfo.email`,
