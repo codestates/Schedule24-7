3a75aa43c17be03e5082c294b278b8d60b73c15c
rec.fffixing <imdex1009@gmail.com>
2021-12-13 11:02:41 +0900

    Feat: Edit Google Oauth API
    클라이언트 서버 업로드에 따른
    구글 Oauth API 리다이렉트 랜딩 수정

--- server/src/modules/auth/auth.service.ts
+++ server/src/modules/auth/auth.service.ts
@@ -252,8 +252,7 @@ export class AuthService {
   async googleCallback(code: string) {
     const GOOGLE_AUTH_URL = "https://accounts.google.com/o/oauth2/v2/auth";
     const GOOGLE_AUTH_TOKEN_URL = "https://oauth2.googleapis.com/token";
-    const GOOGLE_AUTH_REDIRECT_URL =
-      "http://localhost:3000/auth/google/callback";
+    const GOOGLE_AUTH_REDIRECT_URL = "http://schedule24-7.link";
 
     const { data }: any = await lastValueFrom(
       this.httpService.request({
