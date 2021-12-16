f00f196cc568a9b8dc088aa72ac66333cacd4384
SiwooPak <bogus1620@naver.com>
2021-12-16 18:16:23 +0900

    Fix: Add swagger update api's body data - swagger문서에서 유저 비밀번호 변경 부분에 body 데이터 빠진 부분 추가가

--- server/src/modules/user/dto/request/update-user.dto.ts
+++ server/src/modules/user/dto/request/update-user.dto.ts
@@ -1,4 +1,13 @@
-import { PartialType } from "@nestjs/mapped-types";
-import { CreateUserDto } from "./create-user.dto";
+import { IsNotEmpty, IsString } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto {
+  @IsString({ message: "변경할 비밀번호의 타입이 올바르지 않습니다." })
+  @IsNotEmpty({ message: "변경할 비밀번호는 필수값입니다." })
+  @ApiProperty({
+    example: "12345",
+    description: "변경할 비밀번호",
+    required: true,
+  })
+  new_password: string;
+}
