8806a7e770fb28b4888ba565b2d24aed9543ebd6
rec.fffixing <imdex1009@gmail.com>
2021-12-18 16:42:40 +0900

    Feat: edit createGuest API
    게스트 생성 함수 수정
    게스트 계정에 적용될 더미데이터 내용 추가

--- server/src/public/guestUserData.ts
+++ server/src/public/guestUserData.ts
@@ -16,25 +16,39 @@ export function generateGuestUserInfo(): {
   };
 }
 
-export const guestGroupInfo = {
-  groupName: "당직1팀",
-  groupDesc: "당직1팀에 대한 그룹명단",
-  groupEmoji: "😃",
-  works: [
-    {
-      workId: 1,
-      workName: "주간",
-      limit: 2,
-    },
-    {
-      workId: 2,
-      workName: "야간",
-      limit: 2,
-    },
-  ],
-};
+export const guestGroups = [
+  {
+    groupName: "당직1팀",
+    groupDesc: "당직1팀에 대한 그룹명단 (총 7명)(주간/야간 근무 2명씩)",
+    groupEmoji: "😃",
+    works: [
+      {
+        workId: 1,
+        workName: "주간",
+        limit: 2,
+      },
+      {
+        workId: 2,
+        workName: "야간",
+        limit: 2,
+      },
+    ],
+  },
+  {
+    groupName: "개발팀",
+    groupDesc: "개발팀 인원들에 대한 테스트 용 그룹!",
+    groupEmoji: "😃",
+    works: [
+      {
+        workId: 1,
+        workName: "재택근무",
+        limit: 4,
+      },
+    ],
+  },
+];
 
-export const membersInfoList = [
+export const membersList1 = [
   {
     memberName: "김대리",
     memberPosition: "대리",
@@ -72,14 +86,67 @@ export const membersInfoList = [
   },
 ];
 
-export const conditionInfoList = [
+export const membersList2 = [
   {
-    conditionName: "주간 연속 3회 야간 불가",
-    conditionDesc: "한주에 6번 나이트 불가",
-    target: "all",
-    cycle: "weekly",
-    workId: 2,
-    operation: "<",
-    value: 6,
+    memberName: "김코딩",
+    memberPosition: "1년차",
+    memberVacation: [],
   },
+  {
+    memberName: "박해커",
+    memberPosition: "2년차",
+    memberVacation: [],
+  },
+  {
+    memberName: "최구글",
+    memberPosition: "2년차",
+    memberVacation: [],
+  },
+  {
+    memberName: "이애플",
+    memberPosition: "3년차",
+    memberVacation: [],
+  },
+  {
+    memberName: "고삼성",
+    memberPosition: "4년차",
+    memberVacation: [],
+  },
+  {
+    memberName: "이메타",
+    memberPosition: "4년차",
+    memberVacation: [],
+  },
+  {
+    memberName: "김버그",
+    memberPosition: "1년차",
+    memberVacation: [],
+  },
+];
+
+export const membersLists = [membersList1, membersList2];
+
+export const conditionsCollection = [
+  [
+    {
+      conditionName: "주간 연속 3회 야간 불가",
+      conditionDesc: "한주에 3번 나이트 불가",
+      target: "all",
+      cycle: "weekly",
+      workId: 2,
+      operation: "<",
+      value: 6,
+    },
+  ],
+  [
+    {
+      conditionName: "한주에 재택근무 6회 미만",
+      conditionDesc: "한주에 재택 6번은 안됩니다",
+      target: "all",
+      cycle: "weekly",
+      workId: 1,
+      operation: "<",
+      value: 6,
+    },
+  ],
 ];
